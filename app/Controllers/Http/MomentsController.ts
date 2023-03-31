b4cdbe18e7773cc4356937292ee81fbe8ccd7912
Weiller Carvalho <engweiller@hotmail.com>
2023-03-31 17:50:54 -0300

    feat: Controllers

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -5,11 +5,19 @@ import Moment from 'App/Models/Moment'
 export default class MomentsController {
   public async store({ request, response }: HttpContextContract) {
     const body = request.body()
-    const moment = await Moment.create(body)
-    response.status(201)
-    return {
-      message: '201',
-      data: moment,
+    const { title, description } = body
+    if (!title || !description) {
+      return response.status(400)
+    }
+    try {
+      const moment = await Moment.create(body)
+      response.status(201)
+      return {
+        message: '201',
+        data: moment,
+      }
+    } catch (error) {
+      return response.status(500)
     }
   }
 }
